231d2448e5d6318ad159036c03b8ba95401002f0
lkc263 <lkc263@naver.com>
2021-10-02 18:07:49 +0900

    do done

--- gdproject/settings.py
+++ gdproject/settings.py
@@ -87,6 +87,7 @@ WSGI_APPLICATION = 'gdproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default' : {
         'ENGINE': 'django.db.backends.sqlite3',
