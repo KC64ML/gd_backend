be32c296bb8fb3aaecc795d6b112b952a692690f
d3fau1t <aciddust20@gmail.com>
2021-10-03 17:43:08 +0900

    Add, new database info

---
+++ gdproject/settings_heroku.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from gdproject.settings import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'heroku_d219501a8ebf168',
+        'USER': 'bfc96653faaba1',
+        'PASSWORD': 'dfc6fc80',
+        'HOST': 'us-cdbr-east-04.cleardb.com',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
+        'CONN_MAX_AGE': 5,
+    }
+}
