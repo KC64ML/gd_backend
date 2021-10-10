6e51a9e05fc5462168497546ef491bcc142f0bea
lkc263 <lkc263@naver.com>
2021-10-10 20:47:45 +0900

    input makemigrations

--- gdproject/urls.py
+++ gdproject/urls.py
@@ -19,6 +19,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('login/', views.login),
+
     path('account/',include('allauth.urls')),
+
+    
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
